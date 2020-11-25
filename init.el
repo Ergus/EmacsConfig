7435d12b290b0755c57fcaec99c6d8c68cbfb911
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2020-11-25 23:53:14 +0100

    Simplify go-mode config.

--- init.el
+++ init.el
@@ -890,14 +890,7 @@ non-nil and probably assumes that `c-basic-offset' is the same as
 ;;__________________________________________________________
 ;; Go languaje
 (use-package go-mode
-  :mode "\\.go\\'"
-  :config
-  (use-package company-go
-    :after company
-    :config
-    (add-to-list (make-local-variable 'company-backends) 'company-go))
-
-  (use-package go-snippets))
+  :mode "\\.go\\'")
 
 ;;__________________________________________________________
 ;; lua language
