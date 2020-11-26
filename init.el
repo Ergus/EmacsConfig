0ad8dc880bcec663cd235bb646c69644d27b7638
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2020-11-26 20:09:20 +0100

    Re-enable automatic delayed company idle

--- init.el
+++ init.el
@@ -1005,7 +1005,7 @@ non-nil and probably assumes that `c-basic-offset' is the same as
 	 )
   :hook ((prog-mode message-mode conf-mode) . company-mode)
   :custom
-  (company-idle-delay nil)	 ;; no delay for autocomplete
+  (company-idle-delay 1.0)	 ;; no delay for autocomplete
   (company-minimum-prefix-length 2)
   (company-selection-wrap-around nil)
   (company-show-numbers t)
