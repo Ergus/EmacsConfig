ff5089dc0b11e6181872fa0485b9d7286abf3cec
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2020-12-03 17:24:42 +0100

    Improve the time measurement
    Use before-init-time instead of my own variable.
    Print a more detailed abstract about times including gc calls.

--- early-init.el
+++ early-init.el
@@ -13,28 +13,26 @@
 ;;
 ;;; Code:
 
-(defconst my/start-time (current-time))
-
-(defmacro mt (&rest body)
-  "Measure the time it takes to evaluate BODY."
-  `(let ((time (current-time)))
-     ,@body
-     (message "%.06f" (float-time (time-since time)))))
-
 (defvar file-name-handler-alist-old file-name-handler-alist)
 
-(setq file-name-handler-alist nil
-      message-log-max 16384
-      gc-cons-threshold most-positive-fixnum   ;; Defer Garbage collection
-      gc-cons-percentage 1.0)
+(setq-default file-name-handler-alist nil
+	      message-log-max 16384
+	      gc-cons-threshold most-positive-fixnum   ;; Defer Garbage collection
+	      gc-cons-percentage 1.0)
 
 (add-hook 'window-setup-hook
           (lambda ()
-            (setq file-name-handler-alist file-name-handler-alist-old
-                  gc-cons-threshold 800000
-                  gc-cons-percentage 0.1)
-	    (garbage-collect)
-	    (message "Load time %.06f" (float-time (time-since my/start-time))))
+            (setq-default file-name-handler-alist file-name-handler-alist-old
+			  gc-cons-threshold 800000
+			  gc-cons-percentage 0.1)
+	    ;; (garbage-collect)
+	    (let ((curtime (current-time)))
+
+	      (message "Times: init:%.06f total:%.06f gc-done:%d"
+		       (float-time (time-subtract after-init-time before-init-time))
+		       (float-time (time-subtract curtime before-init-time))
+		       gcs-done)
+	      ))
 	  t)
 
 (if (fboundp 'tool-bar-mode)
