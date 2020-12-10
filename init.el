8941bde09497b64550560892bda8efa3aa37d821
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2020-12-10 19:08:23 +0100

    Remove ivy-highlight-thing-at-point.
    There is a version for this now in ivy action.

--- init.el
+++ init.el
@@ -1468,19 +1468,6 @@ non-nil and probably assumes that `c-basic-offset' is the same as
   ;;(ivy-height 10)
   ;;(ivy-wrap t)					;; cycle in minibuffer
   :config
-  (defun ivy-highlight-thing-at-point ()
-    "Highlight thing at point or region."
-    (interactive)
-    (require 'hi-lock)
-    (let ((thing (ivy-thing-at-point))
-	  (face (hi-lock-read-face-name)))
-      (or (facep face) (setq face 'hi-yellow))
-      (unless hi-lock-mode (hi-lock-mode 1))
-      (when (use-region-p)
-	(deactivate-mark))
-      (hi-lock-set-pattern (regexp-quote thing) face)))
-  (global-set-key [remap highlight-symbol-at-point] #'ivy-highlight-thing-at-point)
-
   ;; Highlight with arrows by default.
   ;;(add-to-list 'ivy-format-functions-alist '(t . ivy-format-function-arrow))
   (ivy-mode 1))
