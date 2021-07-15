242b957cac7b2a75a29c4e8d3f172a0547ca71ed
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2021-07-15 14:02:11 +0200

    Small changes in the avy config.

--- init.el
+++ init.el
@@ -1962,13 +1962,13 @@ non-nil and probably assumes that `c-basic-offset' is the same as
   (global-set-key (kbd "C-'") avy-basic-map)
   (which-key-add-key-based-replacements "C-'" "avy")
   ;; (avy-timeout-seconds 0.75)
-  (setq-default ;;avy-style 'at        ;; default 'at-full
-		avy-all-windows nil    ;; commands only in this window
-		avy-all-windows-alt t  ;; with prefix commands in all windows
-		avy-case-fold-search nil
+  (setq-default ;;avy-style 'at             ;; default 'at-full
+		avy-all-windows nil         ;; commands only in this window
+		avy-all-windows-alt t       ;; with prefix commands in all windows
+		;; avy-case-fold-search nil ;; Non-nil ignore case
 		avy-highlight-first t
-		avy-indent-line-overlay t ;; show highlight after non-whitespace
-		avy-keys (number-sequence ?a ?z) ;; Order of proposals
+		avy-indent-line-overlay t   ;; show highlight after non-whitespace
+		;; avy-keys (number-sequence ?a ?z) ;; Order of proposals
 		)
   :config
   (with-eval-after-load 'repeat
