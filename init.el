c49e7a154c4eb32a08dd345765512bcf33cc4927
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2021-07-20 20:04:24 +0200

    Add some verbose messages when running delayed hooks

--- init.el
+++ init.el
@@ -174,6 +174,10 @@
 			      (lambda (buf)
 				(when (buffer-live-p buf)
 				  (with-current-buffer buf
+				    (when init-file-debug
+				      (message "Running delay hook for %s: %s"
+					       `,mode-name
+					       ,delayhook))
 				    (my/unset-gc)
 				    (run-hooks ',delayhook)
 				    (my/restore-gc))))
