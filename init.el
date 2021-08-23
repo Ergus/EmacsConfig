184f29c16f6efbddbc66dd310a7824011e903643
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2021-08-23 23:58:58 +0200

    Add more avy keys to reduce needed input

--- init.el
+++ init.el
@@ -2019,7 +2019,9 @@ non-nil and probably assumes that `c-basic-offset' is the same as
 		;; avy-case-fold-search nil ;; Non-nil ignore case
 		avy-highlight-first t
 		avy-indent-line-overlay t   ;; show highlight after non-whitespace
-		;; avy-keys (number-sequence ?a ?z) ;; Order of proposals
+		avy-keys '(?z ?x ?c ?v ?b ?n ?m
+			      ?a ?s ?d ?f ?g ?h ?j ?k ?l
+			      ?q ?w ?e ?r ?t ?y ?u ?i ?o ?p) ;; Order of proposals
 		)
   :config
   (with-eval-after-load 'repeat
