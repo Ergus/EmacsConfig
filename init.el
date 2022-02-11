e0380278271ee3abc5eca16bb9ee2fd51469afe2
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2022-02-11 13:46:11 +0100

    Add csv-mode

--- init.el
+++ init.el
@@ -2322,6 +2322,10 @@ non-nil and probably assumes that `c-basic-offset' is the same as
   (add-hook 'json-mode-delay-hook #'flymake-json-load))
 
 ;;__________________________________________________________
+;; csv mode
+(use-package csv-mode :defer t)
+
+;;__________________________________________________________
 ;; Protobuf mode
 (use-package protobuf-mode :defer t)
 
