5404f687d7329960b53a5b0222bc2e2f8312c7c0
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2024-04-01 19:41:49 +0200

    Add corfu-terminal and enable completion-preview-mode
    This seems to work exactly as I wanted it to be and couldn't manage with
    company.

--- init.el
+++ init.el
@@ -527,6 +527,9 @@ M-<left>' and repeat with M-<left>."
 	      completions-sort 'historical          ;; alphabetical + historical
 	      )
 
+;; Completion preview mode
+(completion-preview-mode 1)
+
 (defun my/fido-mode ()
   "Command to toggle fido-vertial-mode and disable completions."
   (interactive)
@@ -1246,8 +1249,38 @@ M-<left>' and repeat with M-<left>."
 	      (when (eldoc--supported-p)
 		(eldoc-mode (if (eglot-managed-p) 1 -1))))))
 
-
 ;; (use-package consult-eglot :defer t)
+
+(use-package corfu
+  ;; Optional customizations
+  :init
+  (setq-default corfu-cycle t)      ;; Enable cycling for `corfu-next/previous'
+  ;; (corfu-auto t)                 ;; Enable auto completion
+  ;; (corfu-separator ?\s)          ;; Orderless field separator
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+
+  ;; Enable Corfu only for certain modes.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+
+  ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
+  ;; be used globally (M-/).  See also the customization variable
+  ;; `global-corfu-modes' to exclude certain modes.
+  :init
+  (global-corfu-mode)
+  (keymap-set corfu-mode-map "M-RET" #'completion-at-point)
+  (keymap-set corfu-map "M-RET" #'corfu-quit)
+  )
+
+(use-package corfu-terminal
+  :config
+  (corfu-terminal-mode +1))
 
 (use-package cape
   :defer 0.2
