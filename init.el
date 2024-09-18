37b003e307eb090d5988813edc559a6e81b4e86b
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2024-09-18 16:34:21 +0200

    Add commented indent-bars config

--- init.el
+++ init.el
@@ -419,6 +419,27 @@ M-<left>' and repeat with M-<left>."
   (whitespace-mode 1)
   (electric-pair-local-mode 1))
 
+;; New indent bars mode ready to test: https://github.com/jdtsmith/indent-bars
+;; (use-package indent-bars
+;;   :config
+;;   (require 'indent-bars-ts) 		; not needed with straight
+;;   :custom
+;;   (indent-bars-treesit-support t)
+;;   (indent-bars-treesit-ignore-blank-lines-types '("module"))
+;;   ;; Add other languages as needed
+;;   (indent-bars-treesit-scope '((python function_definition
+;; 				       class_definition
+;; 				       for_statement
+;; 				       if_statement
+;; 				       with_statement
+;; 				       while_statement)))
+;;   ;; Note: wrap may not be needed if no-descend-list is enough
+;;   ;;(indent-bars-treesit-wrap '((python argument_list parameters ; for python, as an example
+;;   ;;				      list list_comprehension
+;;   ;;				      dictionary dictionary_comprehension
+;;   ;;				      parenthesized_expression subscript)))
+;;   :hook ((python-base-mode yaml-mode) . indent-bars-mode))
+
 (add-hook 'prog-mode-delay-hook #'my/delayed-common-hook)
 (add-hook 'text-mode-delay-hook #'my/delayed-common-hook)
 (add-hook 'conf-mode-delay-hook #'my/delayed-common-hook)
